dbc553914a4bc1fb097c927ab59cee4beeebaba7
Chaz Selph <chaz@seawolfsoftware.io>
2021-02-16 22:49:36 -0500

    Ping Django Rest API from local esp32 client

--- boot.py
+++ boot.py
@@ -1,24 +1,56 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-
-print('Running: boot.py')
-
-
+import json
+import time
 import network
-import sys
+import urequests
+import socket
+import os
+
+# Read config file from file system into json object
+# with open('config.json') as f:
+#     config = json.load(f)
 
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
+# Check config.json has updated credentials
+# if config['ssid'] == '':
+#     assert False, 'config.json ssid value is empty'
 
-ssid = 'Chaz&Annie'
-password = 'Watermelon23'
 
-if not wlan.isconnected():
+def connect_to_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to Wifi ...')
+        wlan.connect('Chaz&Annie', 'Watermelon23')
+        # request = urequests.get('https://telexi.seawolfsoftware.io/api/v1/')
+        # print(request)
+        while not wlan.isconnected():
+            pass
+    print('Network Configuration:', wlan.ifconfig())
 
-    print('Connecting to Wi-Fi...')
 
-    wlan.connect(ssid, password)
+def connect_to_socket():
 
-    while not wlan.isconnected():
-        pass
-print('Network configuration:', wlan.ifconfig())
+
+    url = 'https://telexi.seawolfsoftware.io/api/v1/'
+    _, _, host, path = url.split('/', 3)
+
+
+    while True:
+        s = socket.socket()
+        addr = socket.getaddrinfo('157.230.93.255', 80)[0][-1]
+        s.connect(addr)
+
+        s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+
+        data = s.recv(100)
+
+        time.sleep(3)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+        s.close()
+
+
+connect_to_wifi()
+connect_to_socket()
--- button.py
+++ button.py
@@ -95,3 +95,100 @@ while True:
 wav.close()
 audio_out.deinit()
 print('Done')
+
+
+
+
+
+print('Running: main.py')
+
+
+import sys
+from time import sleep
+from MicroWebSrv2.MicroWebSrv2 import WebRoute, GET
+from MicroWebSrv2.MicroWebSrv2.libs.XAsyncSockets import XAsyncSocketsPool
+from MicroWebSrv2.MicroWebSrv2.microWebSrv2 import MicroWebSrv2
+import ujson
+
+#
+# xasPool = XAsyncSocketsPool()
+# srvHttp = MicroWebSrv2()
+# # srvHttp.BindAddress = ('192.168.0.1', 12345)
+#
+# # TODO make switch to SSL
+# # srvHttps = MicroWebSrv2()
+# # /Users/chaz/miniconda3/ssl
+# # srvHttps.EnableSSL(certFile='cert/host.cert',
+# #                    keyFile='cert/host.key')
+# # srvHttps.StartInPool(xasPool)
+#
+# srvHttp.StartInPool(xasPool)
+#
+# xasPool.AsyncWaitEvents(threadsCount=1)
+#
+#
+# # response = microrequests.get('http://telexi.seawolfsoftware.io/api/v1')
+# # print(type(response))
+#
+# @WebRoute(GET, '/local')
+# def RequestTest(microWebSrv2, request):
+#     request.Response.ReturnOkJSON({
+#         'Method': request.Method,
+#         'Authorization': request.Authorization,
+#         'isSSL': request.IsSSL,
+#         'Client Address': request.UserAddress,
+#         'Host': request.Host,
+#         'Origin': request.Origin,
+#         'Path': request.Path,
+#         'Accept': request.Accept,
+#         'UserAgent': request.UserAgent,
+#         'HttpVer': request.HttpVer,
+#         'QueryString': request.QueryString,
+#         'QueryParams': request.QueryParams,
+#
+#     })
+#
+#
+#
+#
+# try:
+#     while True:
+#         sleep(1)
+#
+# except KeyboardInterrupt:
+#     srvHttp.Stop()
+#     # srvHttps.Stop()
+#     xasPool.StopWaitEvents()
+#
+#
+
+########
+# Ctrl-c to end
+
+# print('LOAD: button.py')
+import sys
+import time
+import machine
+
+
+# def press():
+#
+#     print('Button PRESSED at:' )
+#     time.sleep(5)
+#
+#
+# led = machine.Pin(23, machine.Pin.OUT)
+# button_state = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+#
+# while True:
+#     #  When a button is pressed, the corresponding pin is
+#     #  connected to the ground and its value goes to 0
+#     if button_state.value() == 0:
+#         pass
+#
+#     elif button_state.value() == 1:
+#         press()
+#         led.on()
+#         time.sleep(1)
+
+
