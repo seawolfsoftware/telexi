c2c85690a6a1bd1bdf8df8e5d36cb3b54ca217cf
Chaz Selph <chaz@seawolfsoftware.io>
2021-02-17 12:03:46 -0500

    Save state before nginx changes

--- boot.py
+++ boot.py
@@ -32,34 +32,43 @@ def connect_to_wifi():
     print('Network Configuration:', wlan.ifconfig())
 
 
-def connect_to_socket():
-
-    url = config['server_url']
-    _, _, host, path = url.split('/', 3)
+def start_local():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 80))
+    s.listen(5)
 
     while True:
-        s = socket.socket()
-        addr = socket.getaddrinfo(config['server_ip'], 80)[0][-1]
-        s.connect(addr)
+        # Socket accept()
+        conn, addr = s.accept()
+        print("Got connection from %s" % str(addr))
 
-        s.send(b"GET /%s HTTP/1.1\r\n Host: %s\r\n" % (bytes(path, "utf-8"), bytes(host, "utf-8")))
+        # Socket receive()
+        request = conn.recv(1024)
+        print("")
+        print("")
+        print("Content %s" % str(request))
 
-        # s.send(bytes('GET %s '
-        #              'HTTP/1.1\r\n'
-        #              'Host: %s\r\n\r\n' %
-        #              ('api/v1/', 'telexi.seawolfsoftware.io'), 'utf-8'))
+        # Socket send()
+        request = str(request)
+        # led_on = request.find('/?LED=1')
+        # led_off = request.find('/?LED=0')
+        # if led_on == 6:
+        #     print('LED ON')
+        #     print(str(led_on))
+        #     led.value(1)
+        # elif led_off == 6:
+        #     print('LED OFF')
+        #     print(str(led_off))
+        #     led.value(0)
+        response = web_page()
 
-        # s.send(bytes('GET /api/v1/ HTTP/1.1\r\nHost: telexi.seawolfsoftware.io\r\n\r\n'), 'utf-8')
-        # GET / HTTP / 1.1\r\nHost: www.google.com\r\n\r\n
+        conn.send(bytes('HTTP/1.1 200 OK\n', 'utf-8'))
+        conn.send(bytes('Content-Type: text/html\n', 'utf-8'))
+        conn.send(bytes('Connection: close\n\n', 'utf-8'))
+        conn.sendall(response)
 
-        data = s.recv(1024)
-        time.sleep(1)
-
-        if data:
-            print(str(data, 'utf8'), end='')
-        else:
-            break
-        s.close()
+        # Socket close()
+        conn.close()
 
 
 def web_page():
@@ -75,42 +84,38 @@ def web_page():
     return html_page
 
 
+def connect_to_socket():
+
+    url = config['server_url']
+    _, _, host, path = url.split('/', 3)
+
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        addr = socket.getaddrinfo(config['server_ip'], 443)[0][-1]
+        print(addr)
+        s.connect(addr)
+
+        # method = (bytes('GET /api/v1/', 'utf-8'))
+        # path = (bytes('api/v1/\n', 'utf-8'))
+        # protocol = (bytes('HTTP/1.1\n', 'utf-8'))
+        # encoding = (bytes('Accept-Encoding: gzip, deflate\n', 'utf-8'))
+        # accept = (bytes('Accept: */*\n', 'utf-8'))
+        # accept_language = (bytes('Accept-Language: en-us\n', 'utf-8'))
+        # host = (bytes('Host: telexi.seawolfsoftware.io\n\n', 'utf-8'))
+
+        request = bytes("GET /api/v1/ HTTP/1.1\r\nHost: telexi.seawolfsoftware.io\r\n\r\n", 'utf-8')
+        # request = b"".join([method])
+        s.send(request)
+
+        data = s.recv(1024)
+        time.sleep(1)
+
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+        s.close()
+
+
 connect_to_wifi()
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(5)
-
-while True:
-    # Socket accept()
-    conn, addr = s.accept()
-    print("Got connection from %s" % str(addr))
-
-    # Socket receive()
-    request = conn.recv(1024)
-    print("")
-    print("")
-    print("Content %s" % str(request))
-
-    # Socket send()
-    request = str(request)
-    # led_on = request.find('/?LED=1')
-    # led_off = request.find('/?LED=0')
-    # if led_on == 6:
-    #     print('LED ON')
-    #     print(str(led_on))
-    #     led.value(1)
-    # elif led_off == 6:
-    #     print('LED OFF')
-    #     print(str(led_off))
-    #     led.value(0)
-    response = web_page()
-
-    conn.send(bytes('HTTP/1.1 200 OK\n', 'utf-8'))
-    conn.send(bytes('Content-Type: text/html\n', 'utf-8'))
-    conn.send(bytes('Connection: close\n\n', 'utf-8'))
-    conn.sendall(response)
-
-    # Socket close()
-    conn.close()
+connect_to_socket()
