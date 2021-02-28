dae35d4c1e2f9379d1933a03b94042e02ae275ff
Chaz Selph <chaz@seawolfsoftware.io>
2021-02-28 17:04:47 -0500

    save state of store

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,3 +13,5 @@ urlpatterns = [
     path('auth/', obtain_auth_token)
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
