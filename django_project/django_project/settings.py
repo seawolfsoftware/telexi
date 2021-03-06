3b94997ed4445a6a2b98049dd88fdc80e37ad701
Chaz Selph <chaz@seawolfsoftware.io>
2021-03-05 20:35:44 -0500

    refactors

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -110,9 +110,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- django_project/store/models.py
+++ django_project/store/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+class ProductManager(models.Manager):
+
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -18,8 +24,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    category = models.ForeignKey(Category,
+                                 related_name='product',
+                                 on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.CASCADE,
+                                   related_name='product_creator')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
@@ -29,6 +39,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'Products'
