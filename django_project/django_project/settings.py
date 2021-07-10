766b7cd91c261aeddcb411630b2f2aac12c09483
Chaz Selph <chaz@seawolfsoftware.io>
2021-07-10 12:43:14 -0400

    refactor completed to work with reactjs frontend

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -86,6 +86,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "https://telexi-frontend-e5wzf.ondigitalocean.app",
     "http://telexi-frontend-e5wzf.ondigitalocean.app",
+    "http://127.0.0.1:8000",
+    "http://192.168.0.10:3000",
 ]
 
 ROOT_URLCONF = 'django_project.urls'
